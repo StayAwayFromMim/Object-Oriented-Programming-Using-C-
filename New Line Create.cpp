a68473ceea46ed7e07151fb79b1f3e326dea1d55
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-28 19:37:32 +0600

    Create New Line using \n

---
+++ New Line Create.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"My name is Mim\n"; cout<<"Lives in Dhaka Bangladesh\n";
+   cout<<"Northern University Bangladesh";
+
+
+   return 0;
+
+}

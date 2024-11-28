978817bd5095a45f55f78f05df631d7d296b757a
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-28 19:48:35 +0600

    \t is an Escape Sequence.
    In C++, \t is an escape sequence that represents a horizontal tab character. It is used to add a tab space (typically equivalent to moving the cursor to the next tab stop) in text output.

---
+++ Add a Tab Space.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"My name is Mim\tand\tLives in Dhaka Bangladesh\n";
+   cout<<"Northern University Bangladesh";
+
+
+   return 0;
+
+}

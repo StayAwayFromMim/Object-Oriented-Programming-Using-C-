669295ea9ea6bc67df64cd6110dc2eaca5488adc
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-28 21:18:17 +0600

    Get user input with Float variable

---
+++ User Input in C++ 3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;
+
+    cout<<"Enter Two Float Values: "<<endl;
+
+    cin>>a>>b;
+    //>>: Extraction operator, used for input streams like cin
+
+
+    cout<<"The Values are: "<<a<<" and "<<b;
+    //<<: Insertion operator, used for output streams like cout
+
+    return 0;
+
+}

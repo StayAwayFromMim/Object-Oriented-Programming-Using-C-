f231d1d248f3a30585b2cb5b62305184189e3ec0
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-28 21:05:00 +0600

    Get user input with double variable

---
+++ User Input in C++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a,b;
+
+    cin>>a>>b;
+    //>>: Extraction operator, used for input streams like cin
+
+
+    cout<<"The Values are: "<<a<<" and "<<b;
+    //<<: Insertion operator, used for output streams like cout
+
+    return 0;
+
+}

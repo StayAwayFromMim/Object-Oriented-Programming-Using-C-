bd968ec4a089f7575d1b54ae2c0549af043cf2b2
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-28 19:55:51 +0600

    Declaration and initialization of variable
    1. What is Keyword?
    2. How to write Keyword?
    3. what is Variable? 
    4. Rules of writing variables.
    5. Data Types in C++.

---
+++ Keyword, Variable, Data Type in C++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    float b = 20.258;
+    double c = 30.175935;
+    char d = 'M';
+
+    cout<<"The Integer Value is: "<< a <<endl;
+    cout<<"The Floating Value is: "<< b <<endl;
+    cout<<"The Double Value is: "<< c <<endl;
+    cout<<"The Character Value is: "<< d <<endl;
+
+
+return 0;
+}

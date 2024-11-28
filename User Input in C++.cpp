2f73fab7d913cc270086eb4f7ea1368b0be0930a
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-28 20:48:31 +0600

    What is cin Command?
    1. How to get User Input?
    2. How to get input?
    3. How to change Console Design?

---
+++ User Input in C++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin>>a;
+    //>>: Extraction operator, used for input streams like cin
+
+
+    cout<<"The Value is: "<<a;
+    //<<: Insertion operator, used for output streams like cout
+
+    return 0;
+
+}

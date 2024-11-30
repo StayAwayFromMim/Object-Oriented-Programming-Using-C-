d4ecd3c385321260577cd0697f5e20da7cd6ff3f
Mahernaz Akter Mim <nubianmim@gmail.com>
2024-11-30 18:40:45 +0600

    Class and Object-2

---
+++ Program of_OOP.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+
+    public:
+        //variables
+        int id;
+        double gpa;
+        //functions
+
+};
+
+int main()
+
+{
+    Student Mim;
+
+    Mim.id = 2175;
+    Mim.gpa = 3.95;
+
+    cout<< Mim.id <<"   "<< Mim.gpa;
+
+}
